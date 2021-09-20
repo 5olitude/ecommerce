0266f3b7777d33eede17a8f13865a5159516e433
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-09-20 22:54:29 +0530

    success

---
+++ database/databasetup.go
@@ -0,0 +1,35 @@
+package database
+
+//
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DbSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully Connected to the mongodb")
+	return client
+}
+
+var Client *mongo.Client = DbSet()
+
+func UserData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
+	return collection
+
+}
