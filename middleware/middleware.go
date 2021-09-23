f8b940a695b79a2f822a46203ddcd3734bf1aadf
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-09-23 15:10:30 +0530

    added middleware to routes

---
+++ middleware/middleware.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	token "ecommerce/tokens"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization Header Provided")})
+			c.Abort()
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_Name)
+		c.Set("last_name", claims.Last_Name)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
