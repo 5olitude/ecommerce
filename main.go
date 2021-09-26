356c7a831e0c3adc56a32b0a5156e92ac5b9944f
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-09-26 12:04:28 +0530

    Signup

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"ecommerce/middleware"
+	"ecommerce/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+}
