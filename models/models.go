fcc11f93299bde1fa1ce6c6daff05cab1cb173cf
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-09-29 09:47:03 +0530

    typo error in models rating fixed

--- models/models.go
+++ models/models.go
@@ -24,6 +24,6 @@ type Product struct {
 	Product_ID   primitive.ObjectID `bson:"_id"`
 	Product_Name *string            `json:"product_name"`
 	Price        *uint64            `json:"price"`
-	Rating       *uint8             `json:"ratig"`
+	Rating       *uint8             `json:"rating"`
 	Image        *string            `json:"image"`
 }
