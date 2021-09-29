e21e505df177c5f179f7b19147dd0183ae0ff06c
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-09-29 12:02:17 +0530

    added query route

--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,7 @@ import (
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/users/signup", controllers.SignUp())
 	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
 }
