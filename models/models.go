e724bed864b95e89955173842cb29f2390b3e4bd
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-10-06 23:22:39 +0530

    added the address struct

--- models/models.go
+++ models/models.go
@@ -7,18 +7,19 @@ import (
 )
 
 type User struct {
-	ID            primitive.ObjectID `json:"_id" bson:"_id"`
-	First_Name    *string            `json:"first_name" validate:"required,min=2,max=30"`
-	Last_Name     *string            `json:"last_name"  validate:"required,min=2,max=30"`
-	Password      *string            `json:"password"   validate:"required,min=6"`
-	Email         *string            `json:"email"      validate:"email,required"`
-	Phone         *string            `json:"phone"      validate:"required"`
-	Token         *string            `json:"token"`
-	Refresh_Token *string            `josn:"refresh_token"`
-	Created_At    time.Time          `json:"created_at"`
-	Updated_At    time.Time          `json:"updtaed_at"`
-	User_ID       string             `json:"user_id"`
-	UserCart      []ProductUser      `json:"usercart" bson:"usercart"`
+	ID              primitive.ObjectID `json:"_id" bson:"_id"`
+	First_Name      *string            `json:"first_name" validate:"required,min=2,max=30"`
+	Last_Name       *string            `json:"last_name"  validate:"required,min=2,max=30"`
+	Password        *string            `json:"password"   validate:"required,min=6"`
+	Email           *string            `json:"email"      validate:"email,required"`
+	Phone           *string            `json:"phone"      validate:"required"`
+	Token           *string            `json:"token"`
+	Refresh_Token   *string            `josn:"refresh_token"`
+	Created_At      time.Time          `json:"created_at"`
+	Updated_At      time.Time          `json:"updtaed_at"`
+	User_ID         string             `json:"user_id"`
+	UserCart        []ProductUser      `json:"usercart" bson:"usercart"`
+	Address_Details []Address          `json:"address" bson:"address"`
 }
 
 type Product struct {
