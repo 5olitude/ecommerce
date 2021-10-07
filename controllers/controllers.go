16412b3a061d70d0f1ed187641f6fa6a497a5d0c
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-10-07 14:21:22 +0530

    Added the function to delete the all address

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -436,3 +436,32 @@ func EditWorkAddress() gin.HandlerFunc {
 		c.IndentedJSON(200, "Successfully updated the Work Address")
 	}
 }
+
+func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid Search Index"})
+			c.Abort()
+			return
+		}
+		addresses := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wromg")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Successfully Deleted!")
+	}
+}
