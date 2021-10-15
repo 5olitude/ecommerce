38771815d0a01330271fdcf8dfeb26f0bd1aba82
5olitude <josephhermis2020@cs.sjcetpalai.ac.in>
2021-10-15 20:16:06 +0530

    added dependency injection for query push  apis

--- controllers/cart.go
+++ controllers/cart.go
@@ -49,7 +49,7 @@ func (app *Application) AddToCart() gin.HandlerFunc {
 		}
 
 		// Why is this called normal and not userID?
-		userQueryID := c.Query("normal")
+		userQueryID := c.Query("userID")
 		if userQueryID == "" {
 			log.Println("user id is empty")
 			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
@@ -81,30 +81,33 @@ func (app *Application) AddToCart() gin.HandlerFunc {
 //function to remove item from cart
 //GET Request
 //http://localhost:8000/addtocart?id=xxxproduct_id&normal=xxxxxxuser_idxxxxxx
-func RemoveItem() gin.HandlerFunc {
+func (app *Application) RemoveItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		remove_id := c.Query("id")
-		user_id := c.Query("normal")
-		if remove_id == "" || user_id == "" {
-			c.Header("Content-Type", "application/json")
-			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid Query"})
-			c.Abort()
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("product id is inavalid")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
 			return
 		}
-		removed_id, _ := primitive.ObjectIDFromHex(remove_id)
-		usert_id, err := primitive.ObjectIDFromHex(user_id)
-		if err != nil {
-			// Use log instead of fmt for logging
-			log.Println(err)
+
+		userQueryID := c.Query("userID")
+		if userQueryID == "" {
+			log.Println("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("UserID is empty"))
 		}
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		// Again two cancels in a single function.
-		defer cancel()
-		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
-		update := bson.M{"$pull": bson.M{"usercart": bson.M{"_id": removed_id}}}
-		_, err = UserCollection.UpdateMany(ctx, filter, update)
+
+		ProductID, err := primitive.ObjectIDFromHex(productQueryID)
 		if err != nil {
-			c.IndentedJSON(500, "Server Error")
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		err = database.RemoveCartItem(ctx, app.prodCollection, app.userCollection, ProductID, userQueryID)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
 			return
 		}
 		c.IndentedJSON(200, "Successfully removed from cart")
@@ -114,6 +117,8 @@ func RemoveItem() gin.HandlerFunc {
 //function to get all items in the cart and total price
 //GET request
 //http://localhost:8000/listcart?id=xxxxxxuser_idxxxxxxxxxx
+//help to add dependency injection
+//Any nice way to group together this json response?
 func GetItemFromCart() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		user_id := c.Query("id")
@@ -146,7 +151,6 @@ func GetItemFromCart() gin.HandlerFunc {
 		}
 		var listing []bson.M
 		if err = pointcursor.All(ctx, &listing); err != nil {
-			// Why would you panic here?
 			log.Println(err)
 			c.AbortWithStatus(http.StatusInternalServerError)
 		}
@@ -158,131 +162,49 @@ func GetItemFromCart() gin.HandlerFunc {
 	}
 }
 
-func BuyFromCart() gin.HandlerFunc {
+func (app *Application) BuyFromCart() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		user_id := c.Query("id")
-		if user_id == "" {
-			c.Header("Content-Type", "application/json")
-			c.JSON(http.StatusNotFound, gin.H{"error": "Invalid"})
-			c.Abort()
-			return
+		userQueryID := c.Query("id")
+		if userQueryID == "" {
+			log.Panicln("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("UserID is empty"))
 		}
-		usert_id, err := primitive.ObjectIDFromHex(user_id)
-		if err != nil {
-			c.IndentedJSON(500, "Internal Server Error")
-		}
-		var getcartitems models.User
-		var ordercart models.Order
-		ordercart.Order_ID = primitive.NewObjectID()
-		ordercart.Orderered_At = time.Now()
-		ordercart.Order_Cart = make([]models.ProductUser, 0)
-		ordercart.Payment_Method.COD = true
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
-		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$usercart"}}}}
-		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$_id"}, {Key: "total", Value: bson.D{primitive.E{Key: "$sum", Value: "$usercart.price"}}}}}}
-		currentresults, err := UserCollection.Aggregate(ctx, mongo.Pipeline{unwind, grouping})
-
-		ctx.Done()
+		err := database.BuyItemFromCart(ctx, app.userCollection, userQueryID)
 		if err != nil {
-			panic(err)
+			c.IndentedJSON(http.StatusInternalServerError, err)
 		}
-		var getusercart []bson.M
-		if err = currentresults.All(ctx, &getusercart); err != nil {
-			panic(err)
-		}
-		var total_price int32
-		for _, user_item := range getusercart {
-			price := user_item["total"]
-			total_price = price.(int32)
-		}
-		ordercart.Price = int(total_price)
-		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
-		update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "orders", Value: ordercart}}}}
-		_, err = UserCollection.UpdateMany(ctx, filter, update)
-		if err != nil {
-			log.Println(err)
-			c.IndentedJSON(500, "something went wrong")
-		}
-		err = UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: usert_id}}).Decode(&getcartitems)
-		if err != nil {
-			log.Println(err)
-			c.IndentedJSON(500, "something went wrong")
-		}
-		var ktx, kancel = context.WithTimeout(context.Background(), 100*time.Second)
-		defer kancel()
-		filter2 := bson.D{primitive.E{Key: "_id", Value: usert_id}}
-		update2 := bson.M{"$push": bson.M{"orders.$[].order_list": bson.M{"$each": getcartitems.UserCart}}}
-		_, err = UserCollection.UpdateOne(ctx, filter2, update2)
-		if err != nil {
-			c.IndentedJSON(500, "something went wrong")
-		}
-		usercart_empty := make([]models.ProductUser, 0)
-		filtered := bson.D{primitive.E{Key: "_id", Value: usert_id}}
-		updated := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "usercart", Value: usercart_empty}}}}
-		_, err = UserCollection.UpdateOne(ctx, filtered, updated)
-		if err != nil {
-			c.IndentedJSON(500, "Internal Server Errror")
-		}
-
-		ktx.Done()
 		c.IndentedJSON(200, "Successfully Placed the order")
-
 	}
 }
 
-func InstantBuy() gin.HandlerFunc {
+func (app *Application) InstantBuy() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		item_id := c.Query("pid")
-		user_id := c.Query("id")
-		if item_id == "" || user_id == "" {
-			c.Header("Content-Type", "application/json")
-			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid Code"})
-			c.Abort()
-			return
+		UserQueryID := c.Query("userid")
+		if UserQueryID == "" {
+			log.Println("UserID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("UserID is empty"))
 		}
-		itemt_id, err := primitive.ObjectIDFromHex(item_id)
-		if err != nil {
-			log.Println(err)
-			c.IndentedJSON(500, "Internal Server Erroe")
+		ProductQueryID := c.Query("pid")
+		if ProductQueryID == "" {
+			log.Println("Product_ID id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product_id is empty"))
 		}
-		usert_id, err := primitive.ObjectIDFromHex(user_id)
-		if err != nil {
-			log.Println(err)
-			c.IndentedJSON(500, "Internal Server Error")
-		}
-		var product_details models.ProductUser
-		var orders_detail models.Order
-		orders_detail.Order_ID = primitive.NewObjectID()
-		orders_detail.Orderered_At = time.Now()
-		orders_detail.Order_Cart = make([]models.ProductUser, 0)
-		orders_detail.Payment_Method.COD = true
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		// Always use the defer cancel() as close to the creation point as possible.
-		defer cancel()
-		err = ProductCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: itemt_id}}).Decode(&product_details)
-		if err != nil {
-			log.Println(err)
-			// This kind of error is already a lot better for the user.
-			c.IndentedJSON(400, "Something Wrong happened")
-		}
-		orders_detail.Price = product_details.Price
-		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
-		update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "orders", Value: orders_detail}}}}
-		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		productID, err := primitive.ObjectIDFromHex(ProductQueryID)
 		if err != nil {
 			log.Println(err)
 			c.AbortWithStatus(http.StatusInternalServerError)
 			return
 		}
 
-		filter2 := bson.D{primitive.E{Key: "_id", Value: usert_id}}
-		update2 := bson.M{"$push": bson.M{"orders.$[].order_list": product_details}}
-		_, err = UserCollection.UpdateOne(ctx, filter2, update2)
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		// Always use the defer cancel() as close to the creation point as possible.
+		defer cancel()
+		err = database.InstantBuyer(ctx, app.prodCollection, app.userCollection, productID, UserQueryID)
 		if err != nil {
-			log.Println(err)
-			c.IndentedJSON(400, "something wrong happened")
+			c.IndentedJSON(http.StatusInternalServerError, err)
 		}
-		c.IndentedJSON(200, "Successully placed the order ")
+		c.IndentedJSON(200, "Successully placed the order")
 	}
 }
